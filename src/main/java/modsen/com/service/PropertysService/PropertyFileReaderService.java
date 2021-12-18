a932d1cef24c5d37d7adf1334017650842c6a703
ivan.mamai00@mail.ru <250219As>
2021-12-18 15:45:58 +0300

    add one line("\n") :)

--- src/main/java/modsen/com/service/PropertysService/PropertyFileReaderService.java
+++ src/main/java/modsen/com/service/PropertysService/PropertyFileReaderService.java
@@ -8,9 +8,9 @@ public class PropertyFileReaderService implements ReaderPropertyFile {
 
     @Override
     public String read(String path, String key) throws IOException {
-        FileInputStream fileInputStream;
         Properties properties = new Properties();
-        fileInputStream = new FileInputStream(path);
+        FileInputStream fileInputStream = new FileInputStream(path);
+
         properties.load(fileInputStream);
         return properties.getProperty(key);
     }
