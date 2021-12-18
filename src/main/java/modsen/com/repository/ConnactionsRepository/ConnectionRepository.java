4c501ecca3f4330cb322880c2bf4a159cc960683
ivan.mamai00@mail.ru <250219As>
2021-12-18 15:45:58 +0300

    simple class for connection done

--- src/main/java/modsen/com/repository/ConnactionsRepository/ConnectionRepository.java
+++ src/main/java/modsen/com/repository/ConnactionsRepository/ConnectionRepository.java
@@ -1,5 +1,35 @@
 package modsen.com.repository.ConnactionsRepository;
 
+import modsen.com.service.PropertysService.PropertyFileReaderService;
+import modsen.com.service.PropertysService.ReaderPropertyFile;
+import org.postgresql.Driver;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionRepository {
-    public static final String linkToDatabase = "jdbc:postgresql://localhost:5432/";
+    public static final ConnectionRepository CONNECTION_REPOSITORY = new ConnectionRepository();
+    private String pathToDatabase;
+    private final String login = "postgres";
+    private final String password = "22k67s4A";
+
+    public ConnectionRepository() {
+        ReaderPropertyFile readerPropertyFile = new PropertyFileReaderService();
+        try {
+            this.pathToDatabase = readerPropertyFile.read("D:\\Java\\JavaProjects\\projects\\Modsen\\beerShop\\src\\main\\resources\\config\\config.properties", "pathToDatabase");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        Driver driver = new Driver();
+        return DriverManager.getConnection(pathToDatabase,login,password);
+    }
+
+    public static ConnectionRepository getInstance() {
+        return CONNECTION_REPOSITORY;
+    }
 }
