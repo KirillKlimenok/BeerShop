b75e89603066fbcb3be5b88061dbd1383c66816d
ivan.mamai00@mail.ru <250219As>
2021-12-18 15:33:11 +0300

    add jsonMapper

--- src/main/java/modsen/com/сontroller/login/LogInServlet.java
+++ src/main/java/modsen/com/сontroller/login/LogInServlet.java
@@ -1,19 +1,33 @@
 package modsen.com.сontroller.login;
 
+import modsen.com.service.UnregisteredUserSevice.UnregisteredUserService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet(name = "LogInServlet", value = "/login")
 public class LogInServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        UnregisteredUserService user =
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    private String getBodyReq(HttpServletRequest req) throws IOException {
+        BufferedReader bufferedReader = req.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String str;
+
+        while ((str = bufferedReader.readLine()) != null) {
+            stringBuilder.append(str).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
