79999ba53bb1fc98710aa7d834e148164497dae3
ivan.mamai00@mail.ru <250219As>
2021-12-18 20:13:25 +0300

    change table user_token and user_list

--- src/main/sql/create/createUsersTables.sql
+++ src/main/sql/create/createUsersTables.sql
@@ -1,12 +1,12 @@
 create table users_token(
     id bigserial primary key not null,
-    token varchar not null
+    token varchar not null,
+    id_user bigint not null
 );
 
 
 create table users_list(
     id bigserial primary key unique not null,
     login varchar(100) unique not null,
-    password_hash varchar not null,
-    id_user_token bigint references users_token unique not null
+    password varchar not null
 );
