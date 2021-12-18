1946e1c6b5532fe01e2d775f832c46deca0b039d
ivan.mamai00@mail.ru <250219As>
2021-12-18 15:33:11 +0300

    add json mapper

--- src/main/java/modsen/com/сontroller/registration/RegistrationServlet.java
+++ src/main/java/modsen/com/сontroller/registration/RegistrationServlet.java
@@ -1,11 +1,15 @@
 package modsen.com.сontroller.registration;
 
+import modsen.com.service.UnregisteredUserSevice.UnregisteredUserService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 
-@WebServlet(name = "RegistrationServlet", value = "/registration")
+@WebServlet(name = "RegistrationServlet", value = "/reg")
 public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,6 +18,19 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String jsonUnregUser = getBodyReq(request);
+        UnregisteredUserService user =
 
+    }
+
+    private String getBodyReq(HttpServletRequest req) throws IOException {
+        BufferedReader bufferedReader = req.getReader();
+        StringBuilder stringBuilder = new StringBuilder();
+        String str;
+
+        while ((str = bufferedReader.readLine()) != null) {
+            stringBuilder.append(str).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
