4d6c996796f343ceee9d5b6ce848e3143d1a20ae
ivan.mamai00@mail.ru <250219As>
2021-12-19 14:52:12 +0300

    done LogInServlet

--- src/main/java/modsen/com/сontroller/login/LogInServlet.java
+++ src/main/java/modsen/com/сontroller/login/LogInServlet.java
@@ -1,5 +1,7 @@
 package modsen.com.сontroller.login;
 
+import modsen.com.repository.UserRepository.UserRepository;
+import modsen.com.service.JsonMapperService.JsonMapperService;
 import modsen.com.service.UnregisteredUserSevice.UnregisteredUserService;
 
 import javax.servlet.*;
@@ -7,16 +9,23 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "LogInServlet", value = "/login")
 public class LogInServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UnregisteredUserService user =
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UnregisteredUserService user = new JsonMapperService().getObj(getBodyReq(request),UnregisteredUserService.class);
+        try {
+            String token = new UserRepository().getUserToken(user);
+            response.sendRedirect("/" + token);
+        } catch (SQLException e) {
+            response.sendError(405,"wrong login or password");
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
     }
 
