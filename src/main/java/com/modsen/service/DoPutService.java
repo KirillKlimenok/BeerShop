cc64befc01628f36ae76f9db63b20b7afd032305
ivan.mamai00@mail.ru <250219As>
2022-01-17 05:58:15 +0300

    optimize import

--- src/main/java/com/modsen/service/DoPutService.java
+++ src/main/java/com/modsen/service/DoPutService.java
@@ -1,13 +1,8 @@
 package com.modsen.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.modsen.exception.AccessException;
 import com.modsen.exception.BeerNotFoundException;
 import com.modsen.exception.BeerParameterNotExistException;
-import com.modsen.exception.DataAlreadyCreatedException;
-import com.modsen.exception.TransactionException;
-import com.modsen.exception.UserNotFoundException;
-import com.modsen.exception.UserRegistrationException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
